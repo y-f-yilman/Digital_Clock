7d09ab216463c15b1bc14be7bda191d435fd0e89
y-f-yilman <y-f-yilman@github.com>
2024-07-30 14:05:49 +0300

    update clock divider to include other Hz values

--- clock_divider_module/clock_divider.v
+++ clock_divider_module/clock_divider.v
@@ -21,17 +21,66 @@
 
 
 module clock_divider(
-input clk,
-output reg clk1hz
-    );
-    
-    reg [26:0] counter;
-    
-    always @(posedge clk)begin
-        counter <= counter +1;
-        if(counter >= 50000000)begin
+  input clk,            // 100MHz clock input
+  input switch_x10,     // 10Hz switch
+  input switch_x100,    // 100Hz switch
+  input switch_x1000,   // 1000Hz switch
+  output reg clk_hz,    // clock output
+      );
+
+    reg clk1hz,    // 1Hz clock output
+    reg clk10hz,   // 10Hz clock output
+    reg clk100hz,  // 100Hz clock output
+    reg clk1000hz  // 1000Hz clock output
+   
+    reg [26:0] counter; // clock counter
+    reg [2:0] switches;   // switch array
+
+
+    casex(switches)
+      3'd0: begin // for 1Hz output
+        always @(posedge clk)begin
+          counter <= counter +1;
+          if(counter >= 50000000)begin
             clk1hz <= ~clk1hz;
             counter <= 0;
+          end
         end
-    end
+        assign clk = clk1hz;
+      end
+
+      3'b1??: begin // for 1000Hz output
+        always @(posedge clk)begin
+          counter <= counter +1;
+          if(counter >= 50000)begin
+            clk1000hz <= ~clk1000hz;
+            counter <= 0;
+          end
+        end
+        assign clk = clk1000hz;
+      end
+
+      3'b01?: begin // for 100Hz output
+        always @(posedge clk)begin
+          counter <= counter +1;
+          if(counter >= 500000)begin
+            clk100hz <= ~clk100hz;
+            counter <= 0;
+          end
+        end
+        assign clk = clk100hz;
+      end
+
+      3'b001: begin // for 10Hz output
+        always @(posedge clk)begin
+          counter <= counter +1;
+          if(counter >= 5000000)begin
+            clk10hz <= ~clk10hz;
+            counter <= 0;
+          end
+        end
+        assign clk = clk10hz;
+      end
+    endcase
+
 endmodule
